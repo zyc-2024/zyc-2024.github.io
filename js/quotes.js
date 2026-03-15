d6a741f4a3fc1c9de1456cddf7b9398502a8e5a1
zyc-2024 <61992011@qq.com>
2026-03-15 20:30:36 +0800

    Prettier

--- js/quotes.js
+++ js/quotes.js
@@ -1,6 +1,6 @@
 let quotes = ["welcome"];
 addEventListener("DOMContentLoaded", function () {
-    let quote = document.getElementById("quotes");
-    let random = Math.floor(Math.random() * quotes.length);
-    quote.innerHTML = quotes[random];
+	let quote = document.getElementById("quotes");
+	let random = Math.floor(Math.random() * quotes.length);
+	quote.innerHTML = quotes[random];
 });
